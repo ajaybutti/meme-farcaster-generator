2cf1c9f3208ac1a964245b4657816cedfb24a772
Ajay butti <ajay>
2024-03-23 17:04:38 +0530

    xmtp added-frog

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -10,6 +10,20 @@ const app = new Frog({
 })
 
 
+app.use("/*",async (c, next) => {
+  await next()
+    const isFrame = c.res.headers.get('content-type')?.includes('html');
+    if (isFrame) {
+      let html = await c.res.text();
+      const metaTag = '<meta property="of:accepts:xmtp" content="2024-02-01" />';
+      html = html.replace(/(<head>)/i, `$1${metaTag}`);
+      c.res = new Response(html, {
+        headers: {
+          'content-type': 'text/html',
+        },
+      });
+    }
+  }); 
 
 
 
